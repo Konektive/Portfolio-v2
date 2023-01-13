afb4c12acdce149126a4e1f169f8917adbc5ae60
Krystian <krystiansporysz@gmail.com>
2023-01-13 18:25:03 +0100

    x

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,33 @@
 import React from "react";
 import styles from "./Main.module.css";
+import reactIcon from "../assets/react.png";
+import jsIcon from "../assets/javascript.jpg";
+import typeIcon from "../assets/typescript.png";
+import dockerIcon from "../assets/docker.png";
+import responiveIcon from "../assets/responsive.jpg.png";
 
 const Main = () => {
   return (
     <div className={styles.main}>
-      <h1>Main</h1>
+      <h1>Technologies I work With!</h1>
+
+      <div className={styles.stack}>
+        <div>
+          <img src={reactIcon} alt="" />
+        </div>
+        <div>
+          <img src={jsIcon} alt="" />
+        </div>
+        <div>
+          <img src={typeIcon} alt="" />
+        </div>
+        <div>
+          <img src={dockerIcon} alt="" />
+        </div>
+        <div>
+          <img src={responiveIcon} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
