242967eacec985f074e76538357e4b623acf0f9e
Krystian <krystiansporysz@gmail.com>
2023-01-24 11:59:24 +0100

    Animation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,35 +1,38 @@
-import React from "react";
 import styles from "./Main.module.css";
 import reactIcon from "../assets/react.png";
 import jsIcon from "../assets/javascript.jpg";
 import typeIcon from "../assets/typescript.png";
 import dockerIcon from "../assets/docker.png";
 import responiveIcon from "../assets/responsive.jpg.png";
-import moon from "../assets/moon.png.jpg";
+import { useInView } from "react-intersection-observer";
 
 const Main = () => {
+  const { ref: stackRef, inView: stackIsVisible } = useInView();
+
   return (
-    <div className={styles.main}>
+    <div className={`${styles.main}`}>
       <h1>Technologies I work With!</h1>
 
-      <div className={styles.stack}>
-        <div>
-          <img src={moon} alt="" />
-        </div>
-        <div>
+      <div ref={stackRef} className={styles.stack}>
+        <div className={`${styles.logo} ${stackIsVisible ? styles.show : ""}`}>
           <img src={reactIcon} alt="" />
+          <h2>React</h2>
         </div>
-        <div>
+        <div className={`${stackIsVisible ? styles.show : ""}`}>
           <img src={jsIcon} alt="" />
+          <h2>JavaScript</h2>
         </div>
-        <div>
+        <div className={`${stackIsVisible ? styles.show : ""}`}>
           <img src={typeIcon} alt="" />
+          <h2>TypeScript</h2>
         </div>
-        <div>
+        <div className={`${stackIsVisible ? styles.show : ""}`}>
           <img src={dockerIcon} alt="" />
+          <h2>Docker</h2>
         </div>
-        <div>
+        <div className={`${stackIsVisible ? styles.show : ""}`}>
           <img src={responiveIcon} alt="" />
+          <h2>RWD</h2>
         </div>
       </div>
     </div>
