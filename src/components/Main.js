10743e43e0b3ebd8db2b1df142e1b651fe0c97a8
Krystian <krystiansporysz@gmail.com>
2023-01-26 14:12:25 +0100

    x

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,26 +11,27 @@ const Main = () => {
 
   return (
     <div className={`${styles.main}`}>
-      <h1>Technologies I work With!</h1>
+      <h1>My set of skills!</h1>
 
       <div ref={stackRef} className={styles.stack}>
         <div className={`${styles.logo} ${stackIsVisible ? styles.show : ""}`}>
           <img src={reactIcon} alt="" />
           <h2>React</h2>
         </div>
-        <div className={`${stackIsVisible ? styles.show : ""}`}>
+
+        <div className={`${styles.logo} ${stackIsVisible ? styles.show : ""}`}>
           <img src={jsIcon} alt="" />
           <h2>JavaScript</h2>
         </div>
-        <div className={`${stackIsVisible ? styles.show : ""}`}>
+        <div className={`${styles.logo} ${stackIsVisible ? styles.show : ""}`}>
           <img src={typeIcon} alt="" />
           <h2>TypeScript</h2>
         </div>
-        <div className={`${stackIsVisible ? styles.show : ""}`}>
+        <div className={`${styles.logo} ${stackIsVisible ? styles.show : ""}`}>
           <img src={dockerIcon} alt="" />
           <h2>Docker</h2>
         </div>
-        <div className={`${stackIsVisible ? styles.show : ""}`}>
+        <div className={`${styles.logo} ${stackIsVisible ? styles.show : ""}`}>
           <img src={responiveIcon} alt="" />
           <h2>RWD</h2>
         </div>
